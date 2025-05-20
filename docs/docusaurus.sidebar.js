0cc501126023bde31696ddc096e7131392b54e88
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-05-20 23:00:37 +1000

    - Hook up newly refactored dialog code within PowerShell side of the project.

--- docs/docusaurus.sidebar.js
+++ docs/docusaurus.sidebar.js
@@ -80,6 +80,7 @@ module.exports = [
     'commands/Register-ADTDll',
     'commands/Remove-ADTContentFromCache',
     'commands/Remove-ADTEdgeExtension',
+    'commands/Remove-ADTEnvironmentVariable',
     'commands/Remove-ADTFile',
     'commands/Remove-ADTFileFromUserProfiles',
     'commands/Remove-ADTFolder',
