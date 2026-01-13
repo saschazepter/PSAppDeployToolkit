e81efc92af27fef91b510af8f4404abe99405b88
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2026-01-13 16:27:37 +1100

    COM-based shortcut WIP test code.

---
+++ src/PSADT/PSADT.LibraryInterfaces/UrlMon.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PSADT.LibraryInterfaces
+{
+    [Guid("FBF23B40-E3F0-101B-8488-00AA003E56F8"), ComImport()]
+    internal partial class InternetShortcut
+    {
+    }
+}
