5654c5099e5f550de0764850d575f1118148a15e
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2026-06-21 13:59:50 +1000

    Fix bad setup regarding new user interface unit tests.
    Each project should have its own test project, not piggybacked onto an existing one just for the PSADT library.

--- src/PSADT/PSADT.UserInterface.Interfaces.Tests/Fluent/AccessibilityLogicTests.cs
+++ src/PSADT/PSADT.UserInterface.Interfaces.Tests/Fluent/AccessibilityLogicTests.cs
@@ -1,29 +1,9 @@
-/*
- * Copyright (C) 2025 Devicie Pty Ltd. All rights reserved.
- *
- * This file is part of PSAppDeployToolkit.
- *
- * PSAppDeployToolkit is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License
- * as published by the Free Software Foundation, either version 3
- * of the License, or (at your option) any later version.
- *
- * PSAppDeployToolkit is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * See the GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with PSAppDeployToolkit. If not, see <https://www.gnu.org/licenses/>.
- */
-
-using System;
+﻿using System;
 using PSADT.UserInterface.DialogResults;
 using PSADT.UserInterface.Interfaces.Fluent;
 using Xunit;
 
-namespace PSADT.Tests
+namespace PSADT.UserInterface.Interfaces.Tests.Fluent
 {
     /// <summary>
     /// Contains smoke tests that verify the test-project wiring between <c>PSADT.Tests</c> and
